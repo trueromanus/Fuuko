38662123b8c6ce2e9f231064858aaf24a55bacc7
TrueRomanus <trueromanus@gmail.com>
2015-12-05 11:13:32 +0300

    + Added files

---
+++ src/HttFluent/Classifiers/ContentDispositionType.cs
@@ -0,0 +1,31 @@
+﻿
+namespace HttFluent.Classifiers {
+	
+	/// <summary>
+	/// Content disposition type.
+	/// </summary>
+	public enum ContentDispositionType {
+
+		/// <summary>
+		/// Not defined.
+		/// </summary>
+		NotDefined = 0,
+
+		/// <summary>
+		/// Inline.
+		/// </summary>
+		Inline = 1,
+
+		/// <summary>
+		/// Attachment.
+		/// </summary>
+		Attachment = 2,
+
+		/// <summary>
+		/// Disposition extension type.
+		/// </summary>
+		DispExtType = 3,
+
+	};
+
+}
---
+++ src/HttFluent/Implementations/HttpResponse.cs
@@ -0,0 +1,31 @@
+﻿using HttFluent.Models.ResponseModels;
+
+namespace HttFluent.Implementations {
+
+	/// <summary>
+	/// HttFluent response.
+	/// </summary>
+	public class HttpResponse : IHttpResponse {
+
+		private readonly ResponseModel m_Response;
+
+		/// <summary>
+		/// Constructor for transfer model.
+		/// </summary>
+		/// <param name="response">Response.</param>
+		public HttpResponse ( ResponseModel response ) {
+			m_Response = response;
+		}
+
+		/// <summary>
+		/// Data.
+		/// </summary>
+		public ResponseModel Response {
+			get {
+				return m_Response;
+			}
+		}
+
+	}
+
+}
