1e5bc7b6fe9bf3b47d9308d8d87706d1b8b7ed7f
trueromanus <trueromanus@gmail.com>
2017-06-20 22:55:02 +0300

    * Moved extensions to another folder

--- src/Extensions/Fuuko.Extensions.NET451/Properties/AssemblyInfo.cs
+++ src/Extensions/Fuuko.Extensions.NET451/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ( "Fuuko" )]
-[assembly: AssemblyDescription ( "Performing HTTP queries in a declarative style" )]
+[assembly: AssemblyTitle("Fuuko.Extensions.NET451")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct ( "Fuuko" )]
-[assembly: AssemblyCopyright ( "Copyright © RomanAsylum 2016" )]
+[assembly: AssemblyProduct("Fuuko.Extensions.NET451")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6579ed6b-18ec-4f13-89b8-182dbf5ab207")]
+[assembly: Guid("6fa57968-5c2a-42bd-8cc3-d70aa2577d2a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "1.2.0.0" )]
-[assembly: AssemblyFileVersion ( "1.2.0.0" )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
