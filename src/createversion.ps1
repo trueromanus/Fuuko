2d2947bfae43378bb022167fb0f3d5c559a7c12f
trueromanus <trueromanus@gmail.com>
2017-11-25 12:03:49 +0300

    * Update script for create version archive

--- src/createversion.ps1
+++ src/createversion.ps1
@@ -3,13 +3,28 @@ $releaseFolder = Join-Path $PSScriptRoot "release"
 # create release directory
 New-Item -ItemType Directory -Force -Path $releaseFolder
 
+function Copy-ToRealeseFolder {
+	param ( [string]$Platform, [string]$ReleaseFolder )
+	
+	$PlatformFolder = Join-Path $ReleaseFolder $Platform
+	
+	New-Item -ItemType Directory -Force -Path $PlatformFolder
+	
+	$BaseFolder = Join-Path $PSScriptRoot $Platform
+	Write-Host $BaseFolder
+	Copy-Item (Join-Path $BaseFolder "Extensions\bin\Release\Fuuko.dll") $PlatformFolder
+	Copy-Item (Join-Path $BaseFolder "Extensions\bin\Release\Fuuko.Extensions.dll") $PlatformFolder
+	Copy-Item (Join-Path $BaseFolder "Extensions\bin\Release\Fuuko.Readers.dll") $PlatformFolder	
+}
+
 #copy files
-Copy-Item (Join-Path $PSScriptRoot "HttFluent\bin\Release\Fuuko.dll") $releaseFolder
-Copy-Item (Join-Path $PSScriptRoot "HttFluent.NET451\bin\Release\Fuuko.NET451.dll") $releaseFolder
-Copy-Item (Join-Path $PSScriptRoot "Fuuko.NET452\bin\Release\Fuuko.NET452.dll") $releaseFolder
-Copy-Item (Join-Path $PSScriptRoot "Fuuko.NET46\bin\Release\Fuuko.NET46.dll") $releaseFolder
-Copy-Item (Join-Path $PSScriptRoot "Fuuko.NET461\bin\Release\Fuuko.NET461.dll") $releaseFolder
-Copy-Item (Join-Path $PSScriptRoot "Fuuko.NET462\bin\Release\Fuuko.NET462.dll") $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.5" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.5.1" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.5.2" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.6" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.6.1" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "NET_4.6.2" -releaseFolder $releaseFolder
+Copy-ToRealeseFolder -platform "UWP" -releaseFolder $releaseFolder
 
 #create archive
 Compress-Archive -Path $releaseFolder -DestinationPath (Join-Path $PSScriptRoot "release.zip") -Force
